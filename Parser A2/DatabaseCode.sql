f69217ac86d8dba298481c59233d127a7a12c477
@RoadToTopProger <petr.ortov@mail.ru>
2022-08-13 04:33:45 +0300

    Код для создания таблицы

---
+++ Parser A2/DatabaseCode.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Firms] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [sellerName]       NVARCHAR (MAX) NOT NULL,
+    [sellerInn]        VARCHAR (15)   NULL,
+    [buyerName]        NVARCHAR (MAX) NOT NULL,
+    [buyerInn]         VARCHAR (20)   NULL,
+    [woodVolumeBuyer]  VARCHAR (25)   NOT NULL,
+    [woodVolumeSeller] VARCHAR (25)   NOT NULL,
+    [dealDate]         DATE           NOT NULL,
+    [dealNumber]       VARCHAR (40)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
